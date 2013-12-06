e17b79f7b91ea9e87562c092d0f63e05830d484a
FSund <filipsu@fys.uio.no>
2013-12-06 10:58:21 +0100

    Rewrote BitMatrix to use vector<vector<BitVector> > instead of a big vector<bitset>.

--- bit_vector.h
+++ bit_vector.h
@@ -1,17 +1,13 @@
 /*
-    Bool/bit vector and 3D bool/bit matrix implemented using std::bitset.
+    Bool/bit vector and 3D bool/bit matrix implemented using std::bitset and std::vector.
 
     Values can be set using operator(), and flipped using flip().
 
     We leave it as an exercise to the reader to implement 2D- or higher-dimensional matrices.
 
-    For range checking in BitMatrix or BitVector, use something like this (note thatbitset[local_bitset_index] won't be out 
-    of range, since the calculation of local_bitset_index above makes sure that local_bitset_index < n_bits_per_bitsets)
-    try {
-        return mem_local.at(vector_index)[local_bitset_index];
-    } catch (...) {
-        cout << "something went wrong" << endl;
-    }
+    The reason for doing this:
+        Having 1000^3 ints in memory would take up 1000^3 * 16 bits = 16 Gbit ~ 1907 MiB
+        Using one bit for each number only takes up          1000^3 =  1 Gbit ~  119 MiB
 */
 
 // #include <iostream>
@@ -23,78 +19,13 @@ using namespace std;
 
 typedef unsigned int uint;
 
-inline int convert_3d_indices_to_linear_index_inline(int x_index, int y_index, int z_index, int n_x, int n_y, int n_z) {
-    /* Converts 3d indices to linear index */
-    return x_index*n_y*n_z + y_index*n_z + z_index;
-}
-
-inline void convert_linear_index_to_3d_indices_inline(int linear_index, int n_x, int n_y, int n_z, int &x_index, int &y_index, int &z_index) {
-    /* Converts linear index to 3d indices */
-    x_index = linear_index/(n_y*n_z);
-    y_index = (linear_index/n_z) % n_y;
-    z_index = linear_index % n_z;
-}
-
-class BitMatrix{
-public:
-    uint n_rows, n_cols, n_slices, n_elem;
-    uint n_bitsets;
-
-private:
-    static const uint n_bits_per_bitset = 8192; // This can/should be tuned, or maybe implemented differently
-    vector<bitset<n_bits_per_bitset> > mem_local;
-
-public:
-    BitMatrix(uint n_rows, uint n_cols, uint n_slices):
-            n_rows(n_rows), 
-            n_cols(n_cols), 
-            n_slices(n_slices), 
-            n_elem(n_rows*n_cols*n_slices), 
-            n_bitsets(uint(ceil(double(n_elem)/n_bits_per_bitset))), // We need at least n_bitsets of size n_bits_per_bitset to store n_elem bits
-            mem_local(vector<bitset<n_bits_per_bitset> > (n_bitsets)) {
-
-        // cout << "n_bitsets = " << n_bitsets << endl;
-        // uint size_of_mem_local = sizeof(mem_local[0])*n_bitsets;
-        // cout << "size of mem_local = " << size_of_mem_local << " bits = ";
-        // if (size_of_mem_local/1024 < 1)
-        //     cout << size_of_mem_local/8 << " bytes.";
-        // else if (size_of_mem_local/pow(1024,2) < 1)
-        //     cout << size_of_mem_local/1024 << " kilobytes.";
-        // else if (size_of_mem_local/pow(1024,3) < 1)
-        //     cout << size_of_mem_local/pow(1024,2) << " megabytes.";
-        // cout << endl;
-    };
-
-    // for non-const BitMatrix
-    inline std::bitset<n_bits_per_bitset>::reference operator()(const int x, const int y, const int z) {
-        uint linear_index = convert_3d_indices_to_linear_index_inline(x, y, z, n_rows, n_cols, n_slices);
-        uint vector_index = linear_index/n_bits_per_bitset;
-        uint local_bitset_index = linear_index % n_bits_per_bitset; // we hope that the compiler uses the remainder from the integer division above for this
-
-        return mem_local[vector_index][local_bitset_index];
-    };
-
-    // for const BitMatrix
-    inline bool operator()(const int x, const int y, const int z) const {
-        uint linear_index = convert_3d_indices_to_linear_index_inline(x, y, z, n_rows, n_cols, n_slices);
-        uint vector_index = linear_index/n_bits_per_bitset;
-        uint local_bitset_index = linear_index % n_bits_per_bitset; // we hope that the compiler uses the remainder from the integer division above for this
-
-        return mem_local[vector_index][local_bitset_index];
-    };
-
-    void flip(const int x, const int y, const int z) {
-        this->operator()(x, y, z) = !(this->operator()(x, y, z));
-    }
-};
-
 class BitVector{
 public:
     uint n_elem;
     uint n_bitsets;
 
 private:
-    static const uint n_bits_per_bitset = 128; // This can/should be tuned, or maybe implemented differently
+    static const uint n_bits_per_bitset = 128; // This can/should be tuned, or maybe implemented differently -- REMEMBER TO CHANGE THIS VALUE IN LINE 81 AS WELL
     vector<bitset<n_bits_per_bitset> > mem_local;
 
 public:
@@ -102,17 +33,26 @@ public:
             n_elem(n_elem), 
             n_bitsets(uint(ceil(double(n_elem)/n_bits_per_bitset))), // We need at least n_bitsets of size n_bits_per_bitset to store n_elem bits
             mem_local(vector<bitset<n_bits_per_bitset> > (n_bitsets)) {
-
-        cout << "n_bitsets = " << n_bitsets << endl;
-        uint size_of_mem_local = sizeof(mem_local[0])*n_bitsets;
-        cout << "size of mem_local = " << size_of_mem_local << " bits" << endl;
     };
+
+    void flip(const int x) {
+        this->operator()(x) = !(this->operator()(x));
+    }
 
     // for non-const BitVector
     inline std::bitset<n_bits_per_bitset>::reference operator()(const int x) {
 
         uint vector_index = x/n_bits_per_bitset;
-        uint local_bitset_index = x % n_bits_per_bitset; // we hope that the compiler uses the remainder from the integer division above for this
+        uint local_bitset_index = x % n_bits_per_bitset; // We hope that the compiler uses the remainder from the integer division above for this
+
+        return mem_local[vector_index][local_bitset_index];
+    };
+
+    // for non-const BitVector
+    inline std::bitset<n_bits_per_bitset>::reference operator[](const int x) {
+
+        uint vector_index = x/n_bits_per_bitset;
+        uint local_bitset_index = x % n_bits_per_bitset;
 
         return mem_local[vector_index][local_bitset_index];
     };
@@ -121,12 +61,54 @@ public:
     inline bool operator()(const int x) const {
 
         uint vector_index = x/n_bits_per_bitset;
-        uint local_bitset_index = x % n_bits_per_bitset; // we hope that the compiler uses the remainder from the integer division above for this
+        uint local_bitset_index = x % n_bits_per_bitset;
 
         return mem_local[vector_index][local_bitset_index];
     };
 
-    void flip(const int x) {
-        this->operator()(x) = !(this->operator()(x));
+    // for const BitVector
+    inline bool operator[](const int x) const {
+
+        uint vector_index = x/n_bits_per_bitset;
+        uint local_bitset_index = x % n_bits_per_bitset;
+
+        return mem_local[vector_index][local_bitset_index];
+    };
+};
+
+class BitMatrix{
+public:
+    uint n_rows, n_cols, n_slices, n_elem;
+    uint n_bitsets;
+
+private:
+    static const uint n_bits_per_bitset = 128; // Should be the same as in BitVector
+    vector<vector<BitVector> > mem_local;
+
+public:
+    BitMatrix(uint n_rows, uint n_cols, uint n_slices):
+            n_rows(n_rows), 
+            n_cols(n_cols), 
+            n_slices(n_slices), 
+            n_elem(n_rows*n_cols*n_slices), 
+            mem_local(vector<vector<BitVector> > (n_rows, vector<BitVector>(n_cols, BitVector(n_slices)))) {
+
+        // cout << "Approximate size of BitMatrix = " << n_elem << " bits = " << double(n_elem)/double(8*1024*1024) << " megabyte" << endl;
+    };
+
+    void flip(const int x, const int y, const int z) {
+        this->operator()(x, y, z) = !(this->operator()(x, y, z));
     }
+
+    // for non-const BitMatrix
+    inline std::bitset<n_bits_per_bitset>::reference operator()(const int x, const int y, const int z) {
+
+        return mem_local[x][y][z];
+    };
+
+    // for const BitMatrix
+    inline bool operator()(const int x, const int y, const int z) const {
+
+        return mem_local[x][y][z];
+    };
 };
--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ typedef unsigned int uint;
 int main(int n_args, char* arg_vec[]) {
 
     uint n = 1000;
-    
-    BitMatrix bitmat(n,n,n);
-    cout << "bitmat(1,2,3) = " << bitmat(1,2,3) << endl;
-    cout << "flipping bit" << endl;
-    bitmat.flip(1,2,3);
-    cout << "bitmat(1,2,3) = " << bitmat(1,2,3) << endl;
 
     BitVector bitvec(n);
     cout << "bitvec(n-1) = " << bitvec(n-1) << endl;
     cout << "flipping bit" << endl;
     bitvec.flip(n-1);
     cout << "bitvec(n-1) = " << bitvec(n-1) << endl;
+
+    BitMatrix bitmat(n,n,n);
+    cout << "bitmat(1,2,3) = " << bitmat(1,2,3) << endl;
+    cout << "flipping bit" << endl;
+    bitmat.flip(1,2,3);
+    cout << "bitmat(1,2,3) = " << bitmat(1,2,3) << endl;
 
     return 0;
 }
